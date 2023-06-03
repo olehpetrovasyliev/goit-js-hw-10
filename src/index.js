1b8bb2b3a40575372f202b7f2f94bf4304cf9379
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2023-06-03 20:37:47 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,18 +13,23 @@ fetchBreeds()
       .map(el => `<option value="${el.id}">${el.name}</option>`)
       .join('');
     refs.select.innerHTML = catListName;
-    fetchCatByBreed(refs.select.value).then(data => {
-      let cat = data[0].breeds[0];
-      refs.catWrapper.innerHTML = `
-          <div class='wrapper'>
-            <img width=500 src="${data[0].url}" alt="${cat.name} loading="lazy">
-            <h2>${cat.name}</h2>
-            <p class="cat-description">${cat.description}</p>
-            <p class="cat-description">Temperament: ${cat.temperament}</p>
-          </div>
+    setTimeout(() => {
+      fetchCatByBreed(refs.select.value)
+        .then(response => {
+          let cat = response[0].breeds[0];
+          console.log(response[0].breeds[0]);
+          refs.catWrapper.innerHTML = `
+                <img width=500 src="${response[0].url}" alt="${cat.name} loading ="lazy">
+                <h2>${cat.name}</h2>
+                <p class="cat-description">${cat.description}</p>
+                <p class="cat-description">Temperament: ${cat.temperament}</p>
         `;
-      refs.loader.classList.add('is-hidden');
-    });
+          refs.loader.classList.add('is-hidden');
+        })
+        .catch(err => {
+          Notiflix.Notify.failure(err);
+        });
+    }, 500);
   })
   .catch(err => {
     Notiflix.Notify.failure(err);
